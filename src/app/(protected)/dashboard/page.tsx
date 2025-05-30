13e9975f2083b533e1bd993054516f8ba4a5e8dd
gamerhsfdm <hsfdm2008@gmail.com>
2025-05-30 20:16:21 -0300

    feat: ensure user has clinic on doctors page

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,25 +1,18 @@
-import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { db } from "@/db";
-import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import SignOutButton from "./_components/sign-out-button";
 
-const Dashboard = async () => {
+const DashboardPage = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
   if (!session?.user) {
     redirect("/authentication");
   }
-  // Preciso pegar as clinicas do usuario
-  const clinics = await db.query.usersToClinicsTable.findMany({
-    where: eq(usersToClinicsTable.userId, session.user.id),
-  });
-  if (clinics.length === 0) {
+  if (!session.user.clinic) {
     redirect("/clinic-form");
   }
   return (
@@ -32,4 +25,4 @@ const Dashboard = async () => {
   );
 };
 
-export default Dashboard;
+export default DashboardPage;
--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,4 +1,6 @@
 import { Plus } from "lucide-react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -10,25 +12,35 @@ import {
   PageHeaderContent,
   PageTitle,
 } from "@/components/ui/page-container";
+import { auth } from "@/lib/auth";
 
-const DoctorsPage = () => {
+const DoctorsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+  if (!session.user.clinic) {
+    redirect("/clinic-form");
+  }
   return (
     <PageContainer>
-        <PageHeader>
-      <PageHeaderContent>
-        <PageTitle>Médicos</PageTitle>
-        <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
-      </PageHeaderContent>
-      <PageActions>
-        <Button>
-          <Plus />
-          Adicionar Médico
-        </Button>
-      </PageActions>
-        </PageHeader>
-    <PageContent>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <Button>
+            <Plus />
+            Adicionar Médico
+          </Button>
+        </PageActions>
+      </PageHeader>
+      <PageContent>
         <h1>Médicos</h1>
-    </PageContent>
+      </PageContent>
     </PageContainer>
   );
 };
