54e115e6f121793b4f600089c6d73c2d28335ddb
gamerhsfdm <hsfdm2008@gmail.com>
2025-05-31 11:51:16 -0300

    feat: add date picker component with nuqs integration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ["date-fns"],
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
--- src/app/(protected)/patients/page.tsx
+++ src/app/(protected)/patients/page.tsx
@@ -1,6 +1,8 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { DataTable } from "@/components/ui/data-table";
 import {
   PageActions,
   PageContainer,
@@ -10,11 +12,14 @@ import {
   PageHeaderContent,
   PageTitle,
 } from "@/components/ui/page-container";
+import { db } from "@/db";
+import { patientsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
-import { DatePicker } from "./_components/date-picker";
+import AddPatientButton from "./_components/add-patient-button";
+import { patientsTableColumns } from "./_components/table-columns";
 
-const DashboardPage = async () => {
+const PatientsPage = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -24,6 +29,9 @@ const DashboardPage = async () => {
   if (!session.user.clinic) {
     redirect("/clinic-form");
   }
+  const patients = await db.query.patientsTable.findMany({
+    where: eq(patientsTable.clinicId, session.user.clinic.id),
+  });
   return (
     <PageContainer>
       <PageHeader>
@@ -34,14 +42,14 @@ const DashboardPage = async () => {
           </PageDescription>
         </PageHeaderContent>
         <PageActions>
-          <DatePicker />
+          <AddPatientButton />
         </PageActions>
       </PageHeader>
       <PageContent>
-        <></>
+        <DataTable data={patients} columns={patientsTableColumns} />
       </PageContent>
     </PageContainer>
   );
 };
 
-export default DashboardPage;
+export default PatientsPage;
